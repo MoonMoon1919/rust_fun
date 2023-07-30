4653cab5318821641da86a61c30cf3640475a241
Maxwell Moon <moon.maxwell@gmail.com>
2023-07-29 21:55:54 -0700

    if let

---
+++ if_let/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    if_let();
+}
+
+fn if_let() {
+    let config_max = Some(3u8);
+
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+}
