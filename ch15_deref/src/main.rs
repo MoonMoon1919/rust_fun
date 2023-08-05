85395af1593e1471eb18009c18e8c4808ba4a933
Maxwell Moon <moon.maxwell@gmail.com>
2023-08-05 09:55:07 -0700

    ch15.2 check-in

---
+++ ch15_deref/src/main.rs
@@ -0,0 +1,35 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!")
+}
+
+fn main() {
+    let x = 5;
+    // let y = &x produces the same result as let y = Box::new(5)
+    // but when using a Box, y is pointing to a copy of x
+    // rather than a reference to x directly
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+}
