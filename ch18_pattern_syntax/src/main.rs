9ddf059ea4e8cdeb688ec4091d417c33a4e923cc
Maxwell Moon <moon.maxwell@gmail.com>
2023-08-09 19:47:39 -0700

    wrap up ch18

--- ch18_pattern_syntax/src/main.rs
+++ ch18_pattern_syntax/src/main.rs
@@ -12,6 +12,15 @@ fn main() {
     assert_eq!(7, y);
 
     destructured_match();
+    match_msg();
+    multiple_destruct();
+    ignore_parts_of_value_match();
+    ignore_remaining_struct_parts();
+    ignore_tuple_parts();
+    match_guard_simple();
+    match_guard_deux();
+    match_guard_tres();
+    bindings();
 }
 
 fn match_literal() {
@@ -80,5 +89,142 @@ fn destructured_match() {
         Point { x, y } => {
             println!("on neither axis: ({x}, {y})");
         }
+    }
+}
+
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(Color),
+}
+
+fn match_msg() {
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message::Quit => {
+            println!("The quit variant has no data to destructure");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and in the y direction {y}");
+        }
+        Message::Write(text) => {
+            println!("Text message: {text}");
+        }
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change the color to red {r}, green {g}, and blue {b}");
+        }
+        Message::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!("Change the color to hue {h}, saturation {s}, and value {v}");
+        }
+        _ => (),
+    }
+}
+
+fn multiple_destruct() {
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+
+    println!(
+        "Feet {} inches {}, point x: {}, point y: {}",
+        feet, inches, x, y
+    );
+}
+
+fn ignore_parts_of_value_match() {
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(5);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
+}
+
+struct NewPoint {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+fn ignore_remaining_struct_parts() {
+    let origin = NewPoint { x: 0, y: 0, z: 0 };
+
+    match origin {
+        NewPoint { x, .. } => {
+            println!("x is {}", x);
+        }
+    }
+}
+
+fn ignore_tuple_parts() {
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {first}, {last}");
+        }
+    }
+}
+
+fn match_guard_simple() {
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+        Some(x) => println!("The number {} is odd", x),
+        None => (),
+    }
+}
+
+fn match_guard_deux() {
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {n}"),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {y}", x);
+}
+
+fn match_guard_tres() {
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
+}
+
+enum NuMessage {
+    Hello { id: i32 },
+}
+
+fn bindings() {
+    let msg = NuMessage::Hello { id: 5 };
+
+    match msg {
+        NuMessage::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {}", id_variable),
+        NuMessage::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        NuMessage::Hello { id } => println!("Found some other id: {}", id),
     }
 }
